d355969273af9a77c715eac4bd420577a820021d
Bat-Amgalan Nasan-Ochir <batamgalan.n@nmma.co>
2021-08-11 17:25:11 +0800

    update

--- ui/cypress/integration/erxes/utils.js
+++ ui/cypress/integration/erxes/utils.js
@@ -29,7 +29,6 @@ export const SignIn = Cypress.Commands.add('signIn', () => {
   cy.get('input[name=email]').type(email);
   cy.get('input[name=password]').type(`${password}{enter}`);
 
-  cy.url().should('include', '/?signedIn');
   cy.url().should('include', '/inbox');
 
   cy.getCookie('auth-token').should('exist');
